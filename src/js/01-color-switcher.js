ddb92cb3aecddef505be998d28f48d3e2215c9cd
Mariia-Ospn <126498309+Mariia-Ospn@users.noreply.github.com>
2023-07-24 22:27:31 +0200

    task 1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,38 @@
+const refs = {
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+};
 
+let interval = null;
+let isActive = false;
+
+refs.startBtn.addEventListener('click', () => {
+  isActive = true;
+  setBtnDisabledState(isActive);
+  interval = setInterval(() => {
+    setBackgroundColor();
+  }, 1000);
+});
+
+refs.stopBtn.addEventListener('click', () => {
+  if (isActive) {
+    isActive = false;
+    setBtnDisabledState(isActive);
+    clearInterval(interval);
+  }
+});
+
+function setBtnDisabledState(active) {
+  refs.startBtn.disabled = active;
+  refs.stopBtn.disabled = !active;
+}
+
+function setBackgroundColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
