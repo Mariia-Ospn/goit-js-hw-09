40028a88863617892d545b439688677deed78757
Mariia-Ospn <126498309+Mariia-Ospn@users.noreply.github.com>
2023-07-26 18:15:29 +0000

    Deploying to gh-pages from @ Mariia-Ospn/goit-js-hw-09@da21210887536b123ea125bf3a6219f276f6ab7e 🚀

--- 01-color-switcher.69f24bd3.js
+++
@@ -1,2 +0,0 @@
-!function(){var t={startBtn:document.querySelector("[data-start]"),stopBtn:document.querySelector("[data-stop]")},n=null,a=!1;function e(n){t.startBtn.disabled=n,t.stopBtn.disabled=!n}t.startBtn.addEventListener("click",(function(){e(a=!0),n=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),t.stopBtn.addEventListener("click",(function(){a&&(e(a=!1),clearInterval(n))}))}();
-//# sourceMappingURL=01-color-switcher.69f24bd3.js.map
